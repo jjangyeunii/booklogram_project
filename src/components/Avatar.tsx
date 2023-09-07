5e338f5dfff0e003268fde2f2c821809ea96d83a
jjangyeunii <euneun0927@naver.com>
2023-09-07 15:57:00 +0900

    feat: avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  size: string;
+  image?: string | null;
+};
+
+export default function Avatar({ size, image }: Props) {
+  return (
+    <div>
+      {/* eslint-disable-next-line @next/next/no-img-element*/}
+      <img
+        className={`w-${size} h-${size} rounded-full`}
+        src={image ?? undefined}
+        alt={`user profile`}
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,14 @@ import { AiOutlineHome } from "react-icons/ai";
 import { RiSearchLine } from "react-icons/ri";
 import { BsPlusSquare } from "react-icons/bs";
 import { signOut, useSession } from "next-auth/react";
+import Avatar from "./Avatar";
 
 const SIGN_BUTTON_CLASS =
   "bg-sky-500 text-white px-2 py-1 rounded-md hover:bg-sky-400";
 
 export default function Navbar() {
   const { data: session } = useSession();
+  const user = session?.user;
   return (
     <nav className="flex justify-between items-center px-6">
       <h1 className="text-3xl font-bold">Booklogram</h1>
@@ -24,6 +26,11 @@ export default function Navbar() {
         <Link href="/new">
           <BsPlusSquare size={24} />
         </Link>
+        {user && (
+          <Link href={`/user/${user.username}`}>
+            <Avatar size="[38px]" image={user.image} />
+          </Link>
+        )}
         {session ? (
           <button className={SIGN_BUTTON_CLASS} onClick={() => signOut()}>
             Sign out
