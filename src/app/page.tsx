b6b64322683ea494381d972eae8cd19aab800854
jjangyeunii <euneun0927@naver.com>
2023-09-08 08:57:03 +0900

    design: homepage 틀잡기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,10 @@ import { authOptions } from "./api/auth/[...nextauth]/route";
 export default async function Home() {
   const session = await getServerSession(authOptions);
   // console.log(session);
-  return <main>Home</main>;
+  return (
+    <section className="w-full flex flex-col md:flex-row px-6">
+      <div className="w-full basis-3/4">Following & Post</div>
+      <div className="basis-1/4">Sidebar</div>
+    </section>
+  );
 }
