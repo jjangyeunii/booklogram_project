ee2e8d35a6da877fa67eb7e3b58431f51fad6c6e
jjangyeunii <euneun0927@naver.com>
2023-09-22 11:07:56 +0900

    design: post card menu button ui

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Simplepost } from "@/model/post";
 import Avatar from "./Avatar";
 import Image from "next/image";
@@ -6,6 +8,8 @@ import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./PostModal";
 import PostDetail from "./PostDetail";
 import ActionBar from "./ActionBar";
+import { useSession } from "next-auth/react";
+import PostMenuButton from "./PostMenuButton";
 
 type Props = {
   post: Simplepost;
@@ -13,14 +17,20 @@ type Props = {
 };
 
 export default function PostListCard({ post, priority = false }: Props) {
+  const { data: session } = useSession();
+  const user = session?.user;
   const { username, userImage, image, booktitle, bookshort, comments } = post;
   const [openModal, setOpenModal] = useState(false);
+  const isMyPost = user?.username === username;
 
   return (
     <article className="w-full flex flex-col justify-center items-center rounded-md bg-neutral-50 shadow-md mr-4 border border-gray-200">
-      <div className="w-full flex items-center p-3 md:py-2">
-        <Avatar image={userImage} size="w-[58px] h-[58px]" />
-        <p className="ml-3 font-bold text-xl">{post.username}</p>
+      <div className="w-full flex justify-between items-center p-3">
+        <div className="flex items-center">
+          <Avatar image={userImage} size="w-[50px] h-[50px]" />
+          <p className="ml-3 font-bold text-xl">{post.username}</p>
+        </div>
+        {isMyPost && <PostMenuButton />}
       </div>
       <Image
         className="w-full object-cover aspect-square"
---
+++ src/components/PostMenuButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { HiDotsVertical } from "react-icons/hi";
+
+export default function PostMenuButton() {
+  return (
+    <button>
+      <HiDotsVertical size={25} />
+    </button>
+  );
+}
